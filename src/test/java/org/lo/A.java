9e30beba29a9c1da6f119b9069e6d8ad4bbaec11
kkskrish <krishnakumar19962@gmail.com>
2022-09-03 23:55:06 +0530

    kumar

--- src/test/java/org/lo/A.java
+++ src/test/java/org/lo/A.java
@@ -15,6 +15,15 @@ public class A {
 	
 		System.out.println("test1");
 		
+		
+	}
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+	@Test
+	public void test3() {
+		System.out.println("test3");
 	}
 
 }
